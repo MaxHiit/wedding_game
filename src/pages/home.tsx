968bedcdff56e68e0976327f358f918921d91afe
MaxHiit <pro.maxcode@gmail.com>
2023-12-24 00:39:47 +0100

    feat(pages): app home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { Button, Heading, Text } from '@/components/ui';
+
+export const Home = () => {
+	return (
+		<>
+			<Heading as='h1' size='3xl'>
+				Do You know the bride
+			</Heading>
+			<Text as='p' className='mt-8 text-center'>
+				Anwser the questions to see how well you know the bride. The person with the most correct answer
+				wins !
+			</Text>
+
+			<Button asChild variant='link' className='mt-8'>
+				<Link to='/register'>Commencer</Link>
+			</Button>
+		</>
+	);
+};
