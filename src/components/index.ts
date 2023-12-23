e27f82a79c53450094551949d6324afb8d127bbb
MaxHiit <pro.maxcode@gmail.com>
2023-12-24 00:44:19 +0100

    feat(pages | components): export all files in global index

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Layout } from './Layout';
+export { Rules } from './Rules';
+export { Questions } from './Questions';
+export { MaskText } from './MaskText';
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { Home } from './home';
+export { Quiz } from './quiz';
+export { Register } from './register';
+export { Score } from './score';
