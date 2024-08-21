85b882a6c465a0a4805ec604f8b005393c4ef395
MaxHiit <pro.maxcode@gmail.com>
2024-08-21 13:40:25 +0200

    feat(assets): add fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,5 +1,18 @@
-import { bodar } from '@/fonts/Bodar';
-import { sfui } from '@/fonts/SFUI';
 import clsx from 'clsx';
+import localFont from 'next/font/local';
+
+export const sfui = localFont({
+	src: '../../public/fonts/SFUIText-Regular.ttf',
+	variable: '--font-sfui',
+	display: 'swap',
+	preload: true
+});
+
+export const bodar = localFont({
+	src: '../../public/fonts/BODAR.ttf',
+	variable: '--font-bodar',
+	display: 'swap',
+	preload: true
+});
 
 export const fonts = { className: clsx(bodar.variable, sfui.variable) };
